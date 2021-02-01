6aaeb209403adfd2a142fb36a9ed269b590fd19c
Matheus Botelho <matheus.botelho97@gmail.com>
2021-02-01 15:07:48 -0300

    Conversão para functional component

--- aula1/likes-and-dislikes-hooks/src/App.js
+++ aula1/likes-and-dislikes-hooks/src/App.js
@@ -1,53 +1,40 @@
-import styles from './App.module.scss'
-
-import React, { Component } from 'react'
+import { useState } from 'react'
 
 import RatingPanel from './components/RatingPanel'
 
-export default class App extends Component {
-  constructor(){
-    super();
-    this.state = {
-      likes: 0,
-      dislikes: 0,
-    };
+import styles from './App.module.scss'
+
+const App = () => {
+  const [likes, setLikes] = useState(0)
+  const [dislikes, setDislikes] = useState(0)
+
+  const like = () => {
+    setLikes(likes + 1)
   }
 
-  //Nunca incremente ou altere um valor no state do componente, toda a alteração deverá ser feita pelo setState 
-  like(){
-    const { likes } = this.state;
-    this.setState({
-      likes: likes + 1,
-    })
+  const dislike = () => {
+    setDislikes(dislikes-1)
   }
 
-  dislike(){
-    const { dislikes } = this.state;
-    this.setState({
-      dislikes: dislikes - 1
-    })
-  }
-
-  render() {
-    const { likes, dislikes } = this.state;
-    return (
-      <div className={styles.container}>
-        <p className={styles.title}>Avaliações</p>
-        <div className={styles.content}>
-          <RatingPanel 
-            onClick={()=> this.dislike()} 
-            label="Negativas" 
-            value={dislikes} 
-            type="dislike"
-          />
-          <RatingPanel 
-            onClick={()=> this.like()} 
-            label="Positivas" 
-            value={likes} 
-            type="like"
-          />
-        </div>
+  return (
+    <div className={styles.container}>
+      <p className={styles.title}>Avaliações</p>
+      <div className={styles.content}>
+        <RatingPanel 
+          onClick={dislike} 
+          label="Negativas" 
+          value={dislikes} 
+          type="dislike"
+        />
+        <RatingPanel 
+          onClick={like} 
+          label="Positivas" 
+          value={likes} 
+          type="like"
+        />
       </div>
-    )
-  }
+    </div>
+  )
 }
+
+export default App
