4fd4ee6ca9961e4b32e1093c674bfdb9c352f41c
Matheus Botelho <matheus.botelho97@gmail.com>
2021-02-01 13:50:28 -0300

    Estlilização contador e adição de botões de adcionar e remover

--- aula1/counter/src/App.js
+++ aula1/counter/src/App.js
@@ -1,13 +1,17 @@
-//import './App.css';
+import './App.css';
 
 import React, { Component } from 'react'
 
 export default class App extends Component {
   render() {
     return (
-      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100%' }}>
-        <p>Contador</p>
-        <p>0</p>
+      <div className="counter-container">
+        <p className="title">Contador</p>
+        <p className="counter">0</p>
+        <div className="buttons-container">
+          <button>Remover</button>
+          <button>Adicionar</button>
+        </div>
       </div>
     )
   }
