a0752d1e5cf6310e3c31b44b53299d6240da735c
Matheus Botelho <matheus.botelho97@gmail.com>
2021-02-01 14:26:04 -0300

    lógica likes/dislikes

--- aula1/likes-and-deslikes/src/App.js
+++ aula1/likes-and-deslikes/src/App.js
@@ -8,42 +8,43 @@ export default class App extends Component {
   constructor(){
     super();
     this.state = {
-      counter: 0
+      likes: 0,
+      dislikes: 0,
     };
   }
 
   //Nunca incremente ou altere um valor no state do componente, toda a alteração deverá ser feita pelo setState 
-  add(){
-    const { counter } = this.state;
+  like(){
+    const { likes } = this.state;
     this.setState({
-      counter: counter + 1
+      likes: likes + 1,
     })
   }
 
-  subtract(){
-    const { counter } = this.state;
+  dislike(){
+    const { dislikes } = this.state;
     this.setState({
-      counter: counter - 1
+      dislikes: dislikes - 1
     })
   }
 
   render() {
-    const { counter } = this.state;
+    const { likes, dislikes } = this.state;
     return (
       <div className="container">
         <p className="title">Avaliações</p>
         <div className="content">
           <div className="ratings-container">
-            <p>Positivas</p>
-            <p className="counter">{counter}</p>
-            <button onClick={()=>this.subtract()}>
+            <p>Negativos</p>
+            <p className="counter">{dislikes}</p>
+            <button onClick={()=>this.dislike()}>
               <FaThumbsDown size={40}/>
             </button>
           </div>
           <div className="ratings-container">
-            <p>Negativos</p>
-            <p className="counter">{counter}</p>
-            <button onClick={()=>this.add()}> 
+            <p>Positivas</p>
+            <p className="counter">{likes}</p>
+            <button onClick={()=>this.like()}> 
               <FaThumbsUp size={40} />
             </button>
           </div>
