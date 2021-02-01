13ea11f4214281ad8a44a96ca5d6f68f4ce81127
Matheus Botelho <matheus.botelho97@gmail.com>
2021-02-01 13:34:45 -0300

    Label Contador e contagem (Visual)

--- aula1/counter/src/App.js
+++ aula1/counter/src/App.js
@@ -1,12 +1,13 @@
-import './App.css';
+//import './App.css';
 
 import React, { Component } from 'react'
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        Hello World!
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+        <p>Contador</p>
+        <p>0</p>
       </div>
     )
   }
