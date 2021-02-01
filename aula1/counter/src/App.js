e62e021880cf9eb7dd1d2c907b5eac062a2f4972
Matheus Botelho <matheus.botelho97@gmail.com>
2021-02-01 13:37:24 -0300

    Height 100% contador container, estilos deletado

--- aula1/counter/src/App.js
+++ aula1/counter/src/App.js
@@ -5,7 +5,7 @@ import React, { Component } from 'react'
 export default class App extends Component {
   render() {
     return (
-      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100%' }}>
         <p>Contador</p>
         <p>0</p>
       </div>
