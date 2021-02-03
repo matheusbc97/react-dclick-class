4ddb9ec3ce2828c06e5a17fd0abbf8b8b8276cc5
Matheus Botelho <matheus.botelho97@gmail.com>
2021-02-02 22:05:56 -0300

    Estrutura Login

--- aula2/social-network/src/pages/Login/index.jsx
+++ aula2/social-network/src/pages/Login/index.jsx
@@ -1,8 +1,33 @@
 /* eslint-disable react/prefer-stateless-function */
 import React, { Component } from 'react';
+import { FaLock, FaUser } from 'react-icons/fa';
+
+import {
+  Container,
+  Content,
+  LoginButton,
+  RegisterButton,
+  Title,
+} from './styles';
 
 export default class Login extends Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <Container>
+        <Content>
+          <Title>Faça o seu login!</Title>
+          <div>
+            <FaUser />
+            <input placeholder="Login" type="password" />
+          </div>
+          <div>
+            <FaLock />
+            <input placeholder="Senha" type="password" />
+          </div>
+          <LoginButton type="submit">Login</LoginButton>
+          <RegisterButton type="button">Cadastre-se</RegisterButton>
+        </Content>
+      </Container>
+    );
   }
 }
