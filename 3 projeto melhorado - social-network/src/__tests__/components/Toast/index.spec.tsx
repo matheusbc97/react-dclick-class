3bd61c4b294480524e3798badc4a171bf3d251dc
Matheus Botelho <matheus.botelho97@gmail.com>
2021-02-24 18:05:21 -0300

    Teste toast retornar null

--- 3 projeto melhorado - social-network/src/__tests__/components/Toast/index.spec.tsx
+++ 3 projeto melhorado - social-network/src/__tests__/components/Toast/index.spec.tsx
@@ -1,34 +1,58 @@
-/* eslint-disable @typescript-eslint/no-empty-function */
 import { render } from '@testing-library/react';
 
+import * as reactRedux from 'react-redux';
 import Toast from '../../../components/Toast';
+
+const mockedDispatch = jest.fn();
 
 jest.mock('react-redux', () => {
   return {
     useDispatch() {
       return {
-        dispatch: jest.fn(),
+        dispatch: mockedDispatch,
       };
     },
-    useSelector() {
-      return {
-        active: true,
-        text: 'Qualquer texto',
-      };
-    },
+    useSelector: jest.fn(),
   };
 });
 
 describe('Toast Component', () => {
+  const useSelectorMock = jest.spyOn(reactRedux, 'useSelector');
+
+  beforeEach(() => {
+    useSelectorMock.mockClear();
+  });
+
   it('Deve renderizar o component Toast', () => {
+    useSelectorMock.mockReturnValue({
+      active: true,
+      text: 'Qualquer texto',
+    });
+
     render(<Toast />);
   });
 
   it('Deve haver o texto no toast', () => {
+    useSelectorMock.mockReturnValue({
+      active: true,
+      text: 'Qualquer texto',
+    });
+
     const { getByTestId } = render(<Toast />);
 
     const containerElement = getByTestId('toast-container');
 
     expect(containerElement).toHaveTextContent('Qualquer texto');
+  });
+
+  it('Deve retornar null', () => {
+    useSelectorMock.mockReturnValue({
+      active: false,
+      text: '',
+    });
+
+    const { container } = render(<Toast />);
+
+    expect(container.innerHTML).toBe('');
   });
 });
